e8c7928258b5a2f717efefeb92e9a8b8f156d6e0
yvonne take <106619470+Yvonne-Aizawa@users.noreply.github.com>
2023-01-08 16:35:21 +0100

    Update tts.py

--- custom_components/picotts_remote/tts.py
+++ custom_components/picotts_remote/tts.py
@@ -94,5 +94,5 @@ class PicoProvider(Provider):
             return (None, None)
 
         if data:
-            return ("mp3",data)
+            return ("mp3",data["data"])
         return (None, None)
